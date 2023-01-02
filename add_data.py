59b0cd01d9f83d0b8de99a59bbcbf40c36752f87
Morjovski <44448092+Morjovski@users.noreply.github.com>
2023-01-02 17:04:29 +0200

    add amount of purchases to next updates

--- add_data.py
+++ add_data.py
@@ -9,13 +9,14 @@ class AddData():
         init(convert=True)
         self.fn = 'data.json'
 
-    def adding_data(self):
+    def add_one_data(self):
         self.d = input(f'Введите дату ({Fore.GREEN}YYYY-MM-DD{Fore.RESET})\n{Fore.RED}Оставьте пустым если дата текущая!{Fore.RESET}: ')
-        self.cash = float(input('Введите прибыль наличными за этот день: '))
-        self.cashless = float(input('Введите прибыль безналичными за этот день: '))
+        self.cash = float(input('Введите прибыль наличными за день: '))
+        self.cashless = float(input('Введите прибыль безналичными за день: '))
+        self.purchases = int(input('Введите количество продаж за день: '))
         if not self.d:
             self.d = str(datetime.datetime.now().date())
-        self.dictionary = {"day": self.d, "cash": self.cash, "cashless": self.cashless}
+        self.dictionary = {"day": self.d, "cash": self.cash, "cashless": self.cashless, "purchases": self.purchases}
         if os.path.exists(self.fn):
             self.update_file()
         else:
@@ -28,13 +29,14 @@ class AddData():
                 break
             c = float(input('Введите прибыль наличными: '))
             csh = float(input('Введите прибыль безналичными: '))
-            dic = {"day": day, "cash": c, "cashless": csh}
+            prch = int(input('Введите количество продаж: '))
+            dic = {"day": day, "cash": c, "cashless": csh, "purchases": prch}
             with open('data.json', 'r+') as f:
                 fd = json.load(f)
                 f.seek(0)
                 fd['data'].append(dic)
                 json.dump(fd, f, indent=4)
-            print(f'Добавлена прибыль {c + csh} за дату {day}')
+            print(f'Добавлена прибыль {c + csh} за дату {day} с {prch} продаж')
 
     def update_file(self):
         with open(self.fn, 'r+') as f:
@@ -42,11 +44,10 @@ class AddData():
             f.seek(0)
             fd["data"].append(self.dictionary)
             json.dump(fd, f, indent=4)
-            print(f'Прибыль {Fore.GREEN}{self.cash + self.cashless}{Fore.RESET} грн за {Fore.GREEN}{self.d}{Fore.RESET} успешно добавлена!')
-            repeat = input('Добавить новую прибыль? (Y/N): ')
+            print(f'{self.purchases} продаж на сумму {Fore.GREEN}{self.cash + self.cashless}{Fore.RESET} грн за {Fore.GREEN}{self.d}{Fore.RESET} успешно добавлена!')
 
     def create_file(self):
         start_file = {"data": [self.dictionary]}
         with open(self.fn, 'w') as f:
             json.dump(start_file, f, indent=4)
-            print(f'Файл {Fore.YELLOW}{self.fn}{Fore.RESET} успешно создан с добавлением прибыли {Fore.GREEN}{self.cash + self.cashless}{Fore.RESET} грн за дату {Fore.GREEN}{self.d}{Fore.RESET}!')
+            print(f'Файл {Fore.YELLOW}{self.fn}{Fore.RESET} успешно создан с добавлением {Fore.GREEN}{self.purchases}{Fore.RESET} продаж на сумму {Fore.GREEN}{self.cash + self.cashless}{Fore.RESET} грн за дату {Fore.GREEN}{self.d}{Fore.RESET}!')
