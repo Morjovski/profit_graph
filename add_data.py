8ad6d580a040fb64069789f6e2ecf7e55466b54d
Morjovski <44448092+Morjovski@users.noreply.github.com>
2023-02-07 18:12:35 +0200

    code enhance

--- add_data.py
+++ add_data.py
@@ -3,6 +3,7 @@ import os
 import datetime
 import colorama
 from colorama import Fore
+from random_data import RandomData
 
 
 class AddData:
@@ -13,16 +14,15 @@ class AddData:
     
     def add_data(self):
         '''Used for adding a new data in data.json'''
-
         while True:
             answ_list = AddData.create_answer()
             dic = {"day": answ_list[0], "cash": answ_list[1], "cashless": answ_list[2], "purchases": int(answ_list[3])}
             if not os.path.exists(self.fn):
                 self.create_file(dic, answ_list[0], answ_list[1], answ_list[2], answ_list[3])
             else:
-                self.update_file(dic, answ_list[0], answ_list[1], answ_list[1], answ_list[3])
-
-    def update_file(self,dic, day, cash, cashless, purchases):
+                self.update_file(dic, answ_list[0], answ_list[1], answ_list[2], answ_list[3])
+            
+    def update_file(self, dic, day, cash, cashless, purchases):
         '''Update data.json by adding new data'''
 
         with open(self.fn, 'r+') as f:
@@ -47,12 +47,17 @@ class AddData:
         answ_list = []
         print(f'Введите "{Fore.RED}q{Fore.RESET}" в любой момент для выхода из режима добавления')
         for index, variable in enumerate(enter):
+            random = input("Введите 'random' для генерации случайных данных: ")
+            if random.lower() == 'random':
+                random_data = RandomData()
+                random_data.randomize()
+                AddData.continue_graph()
             if index == 0:
                 while True:
                     print('Оставьте пустым если дата текущая!')
                     day = input(f'Введите {variable}: ')
                     if day == 'q':
-                        quit()
+                        AddData.continue_graph()
                     try:
                         day = datetime.date(int(day[:4]), int(day[5:7]), int(day[8:]))
                     except ValueError as e:
@@ -67,7 +72,7 @@ class AddData:
                 while True:
                     answ = input(f'Введите {variable}: ')
                     if answ == 'q':
-                        quit()
+                        AddData.continue_graph()
                     try:
                         answ = float(answ)
                     except ValueError:
@@ -76,4 +81,14 @@ class AddData:
                     answ_list.append(answ)
                     break
         return answ_list
+
+    def continue_graph():
+        from main import Mode
+        n = int(input('Вернуться в главное меню? (1) - Да, (0) - Нет: '))
+        if n:
+            n += 1
+            mode = Mode()
+            mode.select()
+        else:
+            quit()
 
