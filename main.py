14d581a63c8ace20bcdc642bd15cfbddba1b6907
Morjovski <44448092+Morjovski@users.noreply.github.com>
2023-02-07 18:09:55 +0200

    catch errors for better UX and right data

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from add_data import AddData
 from graph import Graph
 from create_data import CreateData
@@ -14,44 +16,52 @@ class Mode:
 
     def select(self):
         n = input(f'Ввод прибыли (1)\nСмотреть график (2)): ')
-
         if n == '1':
-            random = input('Для создания случайных значений, введите "random": ')
-            if random.lower() == 'random':
-                self.random.randomize()
-            else:
-                self.ad.add_data()
+            self.ad.add_data()
         elif n == '2':
-            mode = int(input('Просмотр прибыли (0), просмотр кол-ва продаж (1): '))
-            compare = int(input('Сравнить два периода? Да - (1), Нет (0): '))
-            overall = int(
-                input('Общее количество продаж за период? (Да (1), Нет(0)): ' if mode else 'Общая прибыль за период? (Да (1), Нет(0)): ')
-            )
-
-            if compare:
-                per_start = input('Введите начало периода (YYYY-MM): ')
-                per_end = input('Введите конец периода (YYYY-MM): ')
-                self.g.take_period(per_start, per_end)
+            if not os.path.exists('data.json'):
+                print("There is no 'data.json' file! Enter the data below:")
+                self.ad.add_data()
             else:
-                per = input('Какой год\месяц? (YYYY-MM): ')
-                self.g.take_period(per)
+                while True:
+                    try:
+                        mode = int(input('Просмотр прибыли (0), просмотр кол-ва продаж (1): '))
+                        compare = int(input('Сравнить два периода? Да - (1), Нет (0): '))
+                        overall = int(
+                            input('Общее количество продаж за период? (Да (1), Нет(0)): ' if mode else 'Общая прибыль за период? (Да (1), Нет(0)): ')
+                        )
+                        break
+                    except ValueError:
+                        print('Некорректный ввод данных!')
+                    except SyntaxError:
+                        print('Некорректный ввод данных!')
+                    except TypeError:
+                        print('Некорректный ввод данных!')
 
-            self.g.create_data(overall, mode)
+                if compare:
+                    per_start = input('Введите начало периода (YYYY-MM): ')
+                    per_end = input('Введите конец периода (YYYY-MM): ')
+                    self.g.take_period(per_start, per_end)
+                else:
+                    per = input('Какой год\месяц? (YYYY-MM): ')
+                    self.g.take_period(per)
 
-            if mode:
-                if compare:
-                    self.g.create_graph_bar(mode, overall)
-                elif overall:
-                    self.g.create_graph(self.g.overall_list_start)
+                self.g.create_data(overall, mode)
+
+                if mode:
+                    if compare:
+                        self.g.create_graph_bar(mode, overall)
+                    elif overall:
+                        self.g.create_graph(self.g.overall_list_start)
+                    else:
+                        self.g.create_graph(self.g.purchases_start)
                 else:
-                    self.g.create_graph(self.g.purchases_start)
-            else:
-                if compare:
-                    self.g.create_graph_bar(mode, overall)
-                elif overall:
-                    self.g.create_graph(self.g.overall_list_start)
-                else:
-                    self.g.create_graph(self.g.profit_start)
+                    if compare:
+                        self.g.create_graph_bar(mode, overall)
+                    elif overall:
+                        self.g.create_graph(self.g.overall_list_start)
+                    else:
+                        self.g.create_graph(self.g.profit_start)
         else:
             print('Некорректный ввод данных!\n')
             gr.select()
