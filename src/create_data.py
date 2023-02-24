5e4656000c1cccb74a7ec727b8a3e1a50e8d4d4a
Evgeniy <amor3ux@gmail.com>
2023-02-24 22:08:14 +0200

    change directory

---
+++ src/create_data.py
@@ -0,0 +1,244 @@
+import datetime
+from statistics import mean
+
+import db
+import language as lg
+
+
+class CreateData(db.DataBase):
+
+    def __init__(self, LANGUAGE):
+        self.LANGUAGE = LANGUAGE
+        super().__init__(self.LANGUAGE)
+        self.format_data = []
+        self.label = []
+
+    def take_period(self, interval):
+        """Optimise dates for create_data method"""
+
+        db.DataBase.connect(self)
+
+        if interval == 1:
+            self.periods = list(input(lg.enter_years_lang[self.LANGUAGE]).split())
+            if len(self.periods) <= 3:
+                self.periods = list(datetime.datetime.now().strftime('%Y'))
+                print(f'Выбран период за {self.periods[0]} год')
+        elif interval == 2:
+            self.periods = list(input(lg.enter_years_lang[self.LANGUAGE]).split())
+            if len(self.periods) <= 3:
+                self.periods = list(datetime.datetime.now().strftime('%Y'))
+                print(f'Выбран период за {self.periods[0]} год')
+        else:
+            self.periods = list(input(lg.enter_month_lang[self.LANGUAGE]).split())
+            if len(self.periods) <= 6:
+                self.periods = list(datetime.datetime.now().strftime('%Y-%m'))
+                print(f'Выбран период за {self.periods[0]} год')
+        return self.periods
+
+    def create_data(self, interval, overall, mode):
+        """Create data for create_graph bar"""
+
+        if interval == 1:
+            format_data, label = self.collect_years(mode)
+        elif interval == 2:
+            format_data, label = self.collect_months(mode)
+        else:
+            format_data, label = self.collect_days(mode)
+
+        if overall:
+            format_data = self.overall_sum(format_data, interval)
+
+        legend_name, maxval, minval = self.legend_name(self.periods, format_data, interval, mode)
+
+        return format_data, label, legend_name, maxval, minval
+
+    def overall_sum(self, data, interval):
+        """Making data overall by year/month/day"""
+
+        overall_list = []
+
+        for idx, values in enumerate(data):
+            temp = []
+            if interval == 1:
+                if idx == 0:
+                    overall_list.append(values)
+                else:
+                    overall_list.append([overall_list[idx - 1][0] + data[idx][0]])
+            else:
+                for index, value in enumerate(values):
+                    if index == 0:
+                        temp.append(value)
+                    else:
+                        temp.append(temp[index - 1] + values[index])
+                overall_list.append(temp)
+
+        return overall_list
+
+    def collect_years(self, mode):
+        """Collecting and formatting data by years"""
+
+        format_data = []
+        label = []
+
+        for period in self.periods:
+            temp = []
+            year = period[:4]
+            raw_data = self.cur.execute("""SELECT days.day, months.id, years.year, days.cash, days.cashless, days.purchases 
+                                FROM days 
+                                JOIN years 
+                                JOIN months 
+                                ON days.month_id == months.id 
+                                AND days.year_id == years.id
+                                AND years.year == ?""", (year, ))  
+            prepare_data = 0
+            for date in raw_data:
+                if int(period[:4]) == date[2]:
+                    if mode:
+                        prepare_data += date[5]
+                    else:
+                        prepare_data += round(date[3] + date[4], 2)
+            temp.append(prepare_data)
+            label.append(str(year))
+            format_data.append(temp)
+        return format_data, label
+ 
+
+    def collect_months(self, mode):
+        """Collecting and formatting data by months"""
+
+        format_data = []
+
+        for period in self.periods:
+            temp = []
+            label = []            
+            year = period[:4]
+            for month in range(1, 13):
+                raw_data = self.cur.execute("""SELECT days.day, months.id, years.year, days.cash, days.cashless, days.purchases 
+                                FROM days 
+                                JOIN years 
+                                JOIN months 
+                                ON days.month_id == months.id 
+                                AND days.year_id == years.id 
+                                AND months.id == ? 
+                                AND years.year == ?""", (month, year))
+                prepare_data = 0
+                for date in raw_data:
+                    if month == date[1] and int(year) == date[2]:
+                        if mode:
+                            prepare_data += date[5]
+                        else:
+                            prepare_data += round(date[3] + date[4], 2)
+                label.append(str(month))
+                temp.append(prepare_data)
+            format_data.append(temp)
+        return format_data, label
+
+    def collect_days(self, mode):
+        """Collecting and formatting data by days"""
+
+        format_data = []
+
+        for period in self.periods:
+            temp = []
+            label = []
+            year = period[:4]
+            month = period[5:7]
+            raw_data = self.cur.execute("""SELECT days.day, months.id, years.year, days.cash, days.cashless, days.purchases 
+                                FROM days 
+                                JOIN years 
+                                JOIN months 
+                                ON days.month_id == months.id 
+                                AND days.year_id == years.id 
+                                AND months.id == ? 
+                                AND years.year == ?""", (month, year))
+            for date in raw_data:
+                prepare_data = 0
+                if int(month) == date[1] and int(year) == date[2]:
+                    if mode:
+                        prepare_data += date[5]
+                    else:
+                        prepare_data += round(date[3] + date[4], 2)
+                temp.append(prepare_data)
+            format_data.append(temp)
+
+            # Format days for a proper comparsion in graph
+            for l in format_data:
+                if len(l) < 31:
+                    for _ in range(len(l), 32):
+                        l.append(0)
+        
+        # Create ax labels
+        for day in range(1, 32):
+            label.append(str(day))
+
+        return format_data, label
+
+    def average(self, data):
+        """Return average profit or purchases to label"""
+        return round(mean(data), 2)
+    
+    def max_min_value(self, format_data, periods, interval, mode):
+        """Finding max value in formatted data"""
+
+        maxval = 0
+        minval = format_data[0][0]
+        if interval == 3:
+            for periods in self.periods:
+                year = periods[:4]
+                month = periods[5:7]
+                raw_data = self.cur.execute("""SELECT days.day, months.id, years.year, days.cash, days.cashless, days.purchases 
+                                    FROM days 
+                                    JOIN years 
+                                    JOIN months 
+                                    ON days.month_id == months.id 
+                                    AND days.year_id == years.id 
+                                    AND months.id == ? 
+                                    AND years.year == ?""", (month, year))
+                for data in raw_data:
+                    if mode:
+                        if maxval < data[5]:
+                            maxval = data[5]
+                            max_period = [data[2], data[1], data[0]]
+                        if minval > data[5]:
+                            minval = data[5]
+                            min_period = [data[2], data[1], data[0]]
+                    else:
+                        if maxval < float(data[3] + data[4]):
+                            maxval = float(data[3] + data[4])
+                            max_period = [data[2], data[1], data[0]]
+                        if minval > float(data[3] + data[4]):
+                            minval = float(data[3] + data[4])
+                            min_period = [data[2], data[1], data[0]]
+        else:
+            for index, data in enumerate(format_data):
+                if max(data) >= maxval:
+                    maxval = max(data)
+                    max_period = [periods[index], data.index(maxval) + 1]
+                if min(data) <= minval:
+                    minval = min(data)
+                    min_period = [periods[index], data.index(minval) + 1]
+        return maxval, max_period, minval, min_period
+    
+    def legend_name(self, periods, format_data, interval, mode):
+        """Creates legend names for graph"""
+
+        legend_list = []
+        for index, period in enumerate(periods):
+            maxval, best_period, minval, worst_period = self.max_min_value(format_data, periods, interval, mode)
+            if interval == 1:
+                legend = f"{datetime.date(int(period[:4]), 1, 1).strftime('%Y')}, {lg.average_purchases_lang[self.LANGUAGE]} {self.average(format_data[index])}"
+                best_period = datetime.date(int(best_period[0]), 1, 1).strftime('%Y')
+                worst_period = datetime.datetime(int(worst_period[0]), 1, 1).strftime('%Y')
+            elif interval == 2:
+                legend = f"{datetime.date(int(period[:4]), 1, 1).strftime('%Y')}, {lg.average_purchases_lang[self.LANGUAGE]} {self.average(format_data[index])}"
+                best_period = datetime.date(int(best_period[0]), int(best_period[1]), 1).strftime('%B %Y')
+                worst_period = datetime.date(int(worst_period[0]), int(worst_period[1]), 1).strftime('%B %Y')
+            else:
+                legend = f"{datetime.date(int(period[:4]), int(period[5:7]), 1).strftime('%B %Y')}, {lg.average_purchases_lang[self.LANGUAGE]} {self.average(format_data[index])}"
+                best_period = datetime.date(int(best_period[0]), int(best_period[1]), best_period[2])
+                worst_period = datetime.date(int(worst_period[0]), int(worst_period[1]), int(worst_period[2]))
+            legend_list.append(legend)
+        legend_list.append('{0} {1} {2} {3}'.format(lg.max_value_lang[self.LANGUAGE], maxval, lg.max_min_period_lang[self.LANGUAGE], best_period))
+        legend_list.append('{0} {1} {2} {3}'.format(lg.min_value_lang[self.LANGUAGE], minval, lg.max_min_period_lang[self.LANGUAGE], worst_period))
+
+        return legend_list, maxval, minval
---
+++ src/language.py
@@ -0,0 +1,227 @@
+def choose_language():
+    while True:
+        lang = input('Choose a language\nВыберите язык\nОберіть мову\nEN/RU/UA: ')
+        if lang.lower() == 'en' or len(lang) == 0:
+            return "EN"
+        elif lang.lower() == 'ru' or lang.lower() == 'кг':
+            return "RU"
+        elif lang.lower() == 'ua' or lang.lower() == 'гф':
+            return "UA"
+        else:
+            print('Incorrect lang select!\nВведены неправилные данные!\nВведено неправильні дані!')
+
+interval_mode_lang = {
+    "EN": "Build graph by:\nYears at all - (1)\nYear by months - (2)\nMonths by days - (3)",
+    "RU": "Просмотр графика за:\nЗа года в общем - (1)\nЗа года помесячно - (2)\nЗа месяцы по дням - (3)",
+    "UA": "Дивитись графік за:\nЗа роки в цілому - (1)\nЗа роки помісячно - (2)\nЗа місяці по днях - (3)"
+}
+
+interval_mode_input_lang = {
+    "EN": "Enter interval mode: ",
+    "RU": "Введите режим: ",
+    "UA": "Введіть режим: "
+}
+
+input_mode_lang = {
+    "EN": "Enter data (1)\nBuild a graph (2): ",
+    "RU": "Ввод прибыли (1)\nПостроить график (2): ",
+    "UA": "Введеня прибутку (1)\nПобудувати графік (2): "
+}
+
+no_file_data_lang = {
+    "EN": "There is no database file! Enter the data below",
+    "RU": "Файл базы данных ещё не создан! Введите данные ниже для создания",
+    "UA": "Файла бази даних ще не створено! Введіть дані нижче для створення файлу"
+}
+
+purchase_profit_mode_lang = {
+    "EN": "Show profit (0), Show Purchases (1): ",
+    "RU": "Просмотр прибыли (0), просмотр кол-ва продаж (1): ",
+    "UA": "Дивитись прибуток (0), дивитись кількість продаж (1): "
+}
+
+overall_mode_profit_lang = {
+    "EN": "Overall profit at period? Yes (1), No (0): ",
+    "RU": "Общая прибыль за период? Да (1), Нет(0): ",
+    "UA": "Загальний прибуток за період? Так (1), Ні (0): "
+}
+
+overall_mode_purchases_lang = {
+    "EN": "Overall amount of purchases at period? Yes (1), No (0): ",
+    "RU": "Общее количество продаж за период? Да (1), Нет (0): ",
+    "UA": "Загальний кількість продаж за період? Так (1), Ні (0): "
+}
+
+incorrect_data_lang = {
+    "EN": "Incorrect data entry!",
+    "RU": "Некорректный ввод данных!",
+    "UA": "Неправильне введення даних!" 
+}
+
+update_file_lang = {
+    "EN": "Database file successfully updated with data above!",
+    "RU": "Файл базы данных успешно обновлён с введёнными выше значениями!",
+    "UA": "Файл бази даних успішно оновлений з вищевказаними значеннями!"    
+}
+
+create_file_lang = {
+    "EN": "Database file file successfully created with data above! ",
+    "RU": "Файл базы данных успешно создан с введёнными выше значениями! ",
+    "UA": "Файл бази даних успішно створено з вищевказаними значеннями! "    
+}
+
+create_file_enter_lang = {
+    "EN": ['day', 'cash profit', 'cashless profit', 'purchases'],
+    "RU": ['день', 'прибыль наличными', 'прибыль безналичными', 'продажи'],
+    "UA": ['день', 'прибуток готівкою', 'безготівковий прибуток', 'продажі']  
+}
+
+create_file_random_lang = {
+    "EN": "Enter 'random' for generate random data: ",
+    "RU": "Введите 'random' для генерации случайных данных: ",
+    "UA": "Введіть 'random' для генерації довільних даних: "    
+}
+
+leave_empty_lang = {
+    "EN": "Leave blank if the date is today!",
+    "RU": "Оставьте пустым если дата текущая!",
+    "UA": "Не пишіть нічого якщо дата поточна!"    
+}
+
+incorrect_day_lang = {
+    "EN": "Incorrect day entry!\nEnter date in yyyy-mm-dd format!",
+    "RU": "Некорректный ввод даты!\nВведите дату в формате гггг-мм-дд!",
+    "UA": "Неправильне введення дати!\nВведіть дату у форматі рррр-мм-дд!"
+}
+
+answer_enter_lang = {
+    "EN": "Enter",
+    "RU": "Введите",
+    "UA": "Введіть"    
+}
+
+does_not_exist_lang = {
+    "EN": "\nTime period not found in database file!\nReturn to main menu...",
+    "RU": "\nПериод не найден в базе данных!\nВозврат в главное меню...",
+    "UA": "\nВказаний період не знайдено у базі даних!\nВихід у головне меню..."    
+}
+
+profit_label_lang = {
+    "EN": "Profit, c.u.",
+    "RU": "Доход, у.е.",
+    "UA": "Прибуток, у.о."    
+}
+
+purchases_label_lang = {
+    "EN": "Amount of purchases",
+    "RU": "Количество продаж",
+    "UA": "Кількість продажів"    
+}
+
+profit_title_lang = {
+    "EN": "Profit amount in",
+    "RU": "Доход за",
+    "UA": "Прибуток за"    
+}
+
+purchases_title_lang = {
+    "EN": "Amount of purchases in",
+    "RU": "Количество продаж за",
+    "UA": "Кількість продажів за"    
+}
+
+hover_annotation_day_lang = {
+    "EN": "Day: ",
+    "RU": "День",
+    "UA": "День"    
+}
+
+hover_annotation_month_lang = {
+    "EN": "Month:",
+    "RU": "Месяц:",
+    "UA": "Місяць:"    
+}
+
+hover_annotation_year_lang = {
+    "EN": "Year:",
+    "RU": "Год:",
+    "UA": "Рік:"    
+}
+
+hover_annotation_value_lang = {
+    "EN": "Value:",
+    "RU": "Значение:",
+    "UA": "Значення:"    
+}
+
+average_profit_lang = {
+    "EN": "average profit (c.u.):",
+    "RU": "средний доход (у.е.):",
+    "UA": "середній прибуток у.о.):"
+}
+
+average_purchases_lang = {
+    "EN": "average amount:",
+    "RU": "среднее кол-во:",
+    "UA": "середня кількість:"
+}
+
+enter_quit_add_data_lang = {
+    "EN": "Enter 'q' to exit add data mode at any time!",
+    "RU": "Введите 'q' в любой момент для выхода из режима добавления!",
+    "UA": "Введіть 'q' для виходу з режиму додавання в будь-який момент!"    
+}
+
+quit_add_data_lang = {
+    "EN": "Back to main menu? Leave blank empty if No, else (1) - Yes: ",
+    "RU": "Вернуться в главное меню? Оставьте поле пустым если нет, если да - введите что угодно: ",
+    "UA": "Повернутись у головне меню? Залиште поле пустим якщо ні, інакше введіть що завгодно: "
+}
+
+date_already_in_DB = {
+    "EN": "This date is already in database! Period is not added...",
+    "RU": "Дата уже есть в базе данных! Период не добавлен...",
+    "UA": "Ці дані вже існують в базі Дданих! Період не додано..."
+}
+
+enter_years_lang = {
+    "EN": "Enter periods in format (yyyy): ",
+    "RU": "Введите периоды в формате (гггг): ",
+    "UA": "Введіть періоди у форматі (рррр): "
+}
+
+enter_month_lang = {
+    "EN": "Enter periods in format (yyyy-mm): ",
+    "RU": "Введите периоды в формате (гггг-мм): ",
+    "UA": "Введіть періоди у форматі (рррр-мм): "
+}
+
+max_value_lang = {
+    "EN": "Max value is",
+    "RU": "Наибольшее значение",
+    "UA": "Максимальне значення"
+}
+
+min_value_lang = {
+    "EN": "Min value is",
+    "RU": "Наименьшее значение",
+    "UA": "Мінімальне значення"
+}
+
+max_min_period_lang = {
+    "EN": "by period",
+    "RU": "за период",
+    "UA": "за період"
+}
+
+random_year_lang = {
+    "EN": "Enter how much years to generate: ",
+    "RU": "Введите количество лет: ",
+    "UA": "Введіть кількість років: "
+}
+
+randomize_msg_lang = {
+    "EN": "This script generates random values to database, starts from 2020.\nWARNING!\nMaximum amount of years is unlimited!",
+    "RU": "Этот скрипт создаст рандомные значения, начиная с 2020 года.\nВНИМАНИЕ!\nМаксимальное количество лет неограничено!",
+    "UA": "Цей скрипт створює випадкові значення починаючи с 2020 року.\nУВАГА!\nМаксимальная кількість років необмежена!"    
+}
